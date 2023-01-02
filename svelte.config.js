a57fd14ffbaf42173fef705057931cddc815c853
Marcilio Farias <tpmarc@gmail.com>
2023-01-02 18:29:52 -0300

    Fixed assets loading for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: static_adapter()
-	},
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
